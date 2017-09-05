5a6b1264585ac64a1408b180b31aa1ef655bb6db
holenet <holenet@naver.com>
2017-09-06 02:48:56 +0900

    Comment and Delete

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<post_id>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^post/(?P<post_id>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
+	url(r'^post/(?P<post_id>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+	url(r'^comment/(?P<comment_id>[0-9]+)/delete/$', views.comment_delete, name='comment_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 @login_required
 def post_list(request):
@@ -42,3 +42,29 @@ def post_edit(request, post_id):
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form, 'mode':'Edit Post'})
+
+@login_required
+def post_delete(request, post_id):
+	post = get_object_or_404(Post, pk=post_id)
+	post.delete()
+	return redirect('post_list')
+
+@login_required
+def add_comment_to_post(request, post_id):
+	post = get_object_or_404(Post, pk=post_id)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', post_id=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_delete(request, comment_id):
+	comment = get_object_or_404(Comment, pk=comment_id)
+	comment.delete()
+	return redirect('post_detail', post_id=comment.post.pk)
