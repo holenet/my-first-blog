80c0b45e4eded17bf1d33e1bb0cf2328279da1ca
holenet <holenet@naver.com>
2017-09-06 01:18:40 +0900

    여러가지

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm
 
+@login_required
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, post_id):
 	post = get_object_or_404(Post, pk=post_id)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
+def post_new(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', post_id=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form, 'mode':'New Post'})
+
+@login_required
+def post_edit(request, post_id):
+	post = get_object_or_404(Post, pk=post_id)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', post_id=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form, 'mode':'Edit Post'})
